1dc7140d7871496ae1c74bb5e4e329982a8f2f5e
sahracton1 <sta1@st-andrews.ac.uk>
2019-10-22 11:33:19 +0100

    Getting Started with interview cake - Attempted problem #265 --- todo - i need to continue reading about hashmaps!

---
+++ Questions/265/src/LearningHashMAps.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class LearningHashMAps {
+
+
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> lightBulbHoursOfLight = new HashMap<>();
+        lightBulbHoursOfLight.put("incandescent", 1200);
+
+        System.out.println(lightBulbHoursOfLight.size());
+
+    }
+}
